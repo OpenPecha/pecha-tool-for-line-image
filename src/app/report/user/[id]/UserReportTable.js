8f5503e402cf559021048e2eb041d21df59c0744
tentamdin <tgtamdin3@gmail.com>
2024-03-28 15:01:29 +0530

    fix: user report table

--- src/app/report/user/[id]/UserReportTable.js
+++ src/app/report/user/[id]/UserReportTable.js
@@ -1,121 +1,93 @@
-"use client";
-import React, { useEffect, useRef, useState } from "react";
-import { getUserSpecificTasks, getUserSpecificTasksCount } from "@/model/task";
-import PaginationControls from "@/components/PaginationControls";
-import UserReportTable from "./UserReportTable";
-import Select from "@/components/Select";
-import DateInput from "@/components/DateInput";
-import { useRouter, usePathname } from "next/navigation";
+import TiffImageDisplayFromURL from "@/components/TiffImageDisplayFromUrl";
+import Image from "next/image";
+import React from "react";
 
-const UserReport = ({ searchParams, id, users }) => {
-  const [userTaskRecord, setUserTaskRecord] = useState([]);
-  const [totalTasks, setTotalTasks] = useState(0);
-  const [selectedOption, setSelectedOption] = useState(id ? id : "");
-  const [dates, setDates] = useState({ from: "", to: "" });
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-  const pathname = usePathname();
-  const page = searchParams["page"] ?? "1";
-  const per_page = searchParams["per_page"] ?? "10";
-  const isReport = pathname.includes("report");
-  let allUserSpecificTasks = useRef([]);
-
-  // Number of items per page
-  const limit = typeof per_page === "string" ? parseInt(per_page) : 10;
-  // Number of items to skip
-  const skip =
-    typeof page === "string"
-      ? parseInt(page) > 0
-        ? (parseInt(page) - 1) * limit
-        : 0
-      : 0;
-  const end = skip + limit;
-
-  useEffect(() => {
-    setIsLoading(true); // Start loading
-    async function getUserReportByGroup() {
-      try {
-        allUserSpecificTasks.current = await getUserSpecificTasks(
-          selectedOption,
-          limit,
-          skip,
-          dates
-        );
-        const totalUserSpecificTasks = await getUserSpecificTasksCount(
-          selectedOption,
-          dates
-        );
-        setUserTaskRecord(allUserSpecificTasks.current); // Update state with the fetched data
-        setTotalTasks(totalUserSpecificTasks); // Update state with the total count
-      } catch (error) {
-        console.error("Failed to fetch user report by group:", error);
-        // Optionally, handle the error state in your UI as well
-      } finally {
-        setIsLoading(false); // End loading regardless of try/catch outcome
-      }
-    }
-    getUserReportByGroup();
-  }, [selectedOption, skip, limit, dates]);
-
-  const handleOptionChange = async (event) => {
-    setSelectedOption(event.target.value);
-    router.push(`/report/user/${event.target.value}`);
-  };
-
-  const handleDateChange = async (event) => {
-    setDates((prev) => ({ ...prev, [event.target.name]: event.target.value }));
-  };
-
-  const totalTasksCount = totalTasks;
-  const pageCount = Math.ceil(totalTasksCount / limit);
+const UserReportTable = ({ userTaskRecord }) => {
+  function formattedDate(date) {
+    return date.toLocaleString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  }
 
   return (
-    <div className="h-full">
-      <form className="sticky top-0 z-20 p-4 gap-4 bg-white flex flex-col md:flex-row justify-center md:items-end">
-        <Select
-          title="user_id"
-          label="User"
-          options={users}
-          selectedOption={selectedOption}
-          handleOptionChange={handleOptionChange}
-          isReport={isReport}
-        />
-        <div className="flex flex-col md:flex-row gap-4 md:gap-6">
-          <DateInput
-            label="from"
-            selectedDate={dates.from}
-            handleDateChange={handleDateChange}
-            isReport={isReport}
-          />
-          <DateInput
-            label="to"
-            selectedDate={dates.to}
-            handleDateChange={handleDateChange}
-            isReport={isReport}
-          />
-        </div>
-      </form>
-      <div className="flex flex-col justify-center items-center my-10">
-        {isLoading ? (
-          <div className="text-center mt-10">
-            <span className="loading loading-spinner text-success text-center"></span>
-          </div>
-        ) : (
-          <>
-            <UserReportTable userTaskRecord={userTaskRecord} />
-            <PaginationControls
-              page={page}
-              per_page={per_page}
-              hasNextPage={end < totalTasksCount}
-              hasPrevPage={skip > 0}
-              pageCount={pageCount}
-              isReport={isReport}
-            />
-          </>
-        )}
+    <>
+      <div className="overflow-x-auto shadow-md sm:rounded-lg w-11/12 md:w-4/5 max-h-[70vh]">
+        <table className="table table-auto table-pin-rows">
+          {/* head */}
+          <thead className="text-gray-700 bg-gray-50">
+            <tr>
+              <th className="pr-80">Transcript</th>
+              <th>Image</th>
+              <th>State</th>
+              <th>Submitted at</th>
+              <th>Reviewed at</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userTaskRecord.map((task) => (
+              <tr key={task.id}>
+                <td
+                  className={`border-l-4 ${
+                    task.transcriber_is_correct
+                      ? "border-green-500"
+                      : "border-red-500"
+                  }`}
+                >
+                  <div className="grid gap-2 mb-2">
+                    <strong>Submitted:</strong>
+                    {task.transcript}
+                  </div>
+                  {task.reviewed_transcript !== null && (
+                    <div className="grid gap-2 mb-2">
+                      <strong>Reviewed:</strong>
+                      {task.reviewed_transcript}
+                    </div>
+                  )}
+                  {task.final_reviewed_transcript !== null && (
+                    <div className="grid gap-2 mb-2">
+                      <strong>Final:</strong>
+                      {task.final_reviewed_transcript}
+                    </div>
+                  )}
+                </td>
+                <td>
+                  {task.url?.includes(".tif") ? (
+                    <TiffImageDisplayFromURL imageUrl={task.url} />
+                  ) : (
+                    <Image
+                      src={task.url}
+                      alt="image"
+                      width={1500}
+                      height={400}
+                      className="object-cover max-w-[50vw]"
+                      priority={true}
+                    />
+                  )}
+                </td>
+                <td>{task.state}</td>
+
+                <td>
+                  {task.submitted_at !== null
+                    ? formattedDate(task?.submitted_at)
+                    : ""}
+                </td>
+                <td>
+                  {task.reviewed_at !== null
+                    ? formattedDate(task?.reviewed_at)
+                    : ""}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </div>
+    </>
   );
 };
 
-export default UserReport;
+export default UserReportTable;
